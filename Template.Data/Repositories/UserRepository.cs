ecc1d2df661df9cca3568d27d0c809c87367a003
Fernando de Castro <fca.apps@gmail.com>
2021-07-18 12:47:16 -0300

    Quarto Commit

---
+++ Template.Data/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Domain.Interfaces;
+using Template.Data.Repositories;
+using Template.Data.Content;
+using Template.Domain.Entities;
+
+namespace Template.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+
+        public UserRepository(TemplateContext context)
+            : base(context) { }
+
+        public IEnumerable<User> GetAll()
+        {
+            return Query(x => !x.IsDeleted);
+        }
+
+    }
+
+}
