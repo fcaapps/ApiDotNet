c32642e06d03e22f1f4f14883ff222f2d7543c94
unknown <fca.apps@gmail.com>
2021-07-17 23:35:06 -0300

    Terceiro Commit

---
+++ Template.Data/Content/TemplateContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Data.Content
+{
+    class TemplateContext : DbContext
+    {
+        //public TemplateContext(DbContextOptions<TemplateContext> option)
+            //base(option) {}
+    }
+}
---
+++ Template.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Domain.Entities
+{
+    class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
